352f4fd2b6da0262ec8117b8ee88ba11222275f6
Joe Nguwo <nguwojoe@gmail.com>
2026-07-25 00:18:12 +0200

    created the proteced routes component to protect private routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,23 +1,25 @@
 import { Routes, Route } from "react-router-dom";
-
 import Loginpage from "@/pages/login/login";
 import Registerpage from "@/pages/login/register";
 import Dashboard from "@/pages/dashboard";
 import Students from "@/pages/students";
-
 import SidebarLayout from "@/layout/sidebar";
+import ProtectedRoutes from "@/protectedRoutes";
 
 function Routess() {
   return (
     <Routes>
-     
+
       <Route path="/" element={<Loginpage />} />
       <Route path="/register" element={<Registerpage />} />
 
-      <Route path="/auth" element={<SidebarLayout />}>
-        <Route path="dashboard" element={<Dashboard />} />
-        <Route path="students" element={<Students />} />
+      <Route element={<ProtectedRoutes />}>
+        <Route path="/auth" element={<SidebarLayout />}>
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="students" element={<Students />} />
+        </Route>
       </Route>
+
     </Routes>
   );
 }
